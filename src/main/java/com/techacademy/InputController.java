b079024492a4b18a49a051edbd4da23437cb8b15
Oisisushi <osushi.oisisushi@gmail.com>
2024-02-14 10:54:43 +0900

    コメント部分を削除
    課題作成中に記入していたコメント部分を削除

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,10 +1,7 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
@@ -14,9 +11,4 @@ public class InputController {
         return "input";
     }
 
-/*    @PostMapping("/output")
-    public String postOutput(@RequestParam("previous") String previous, Model model) {
-        model.addAttribute("previous", previous);
-        return "output";
-    } */
 }
